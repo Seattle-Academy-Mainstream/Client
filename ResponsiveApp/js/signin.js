eeb2ecfe84c293ba25e6034ee5265b66f3536483
Isaac Benson Zinda <isaaczinda@seattleacademy.org>
2015-08-27 18:09:00 -0700

    update

--- ResponsiveApp/js/signin.js
+++ ResponsiveApp/js/signin.js
@@ -4,4 +4,8 @@ function onSignIn(googleUser) {
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail());
+
+  var id_token = googleUser.getAuthResponse().id_token;
+
+  console.log(id_token);
 }
