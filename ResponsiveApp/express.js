c34a9fdbd9230730dab0778778b15e464a95a4ee
Isaac Benson Zinda <isaaczinda@seattleacademy.org>
2015-11-01 15:54:56 -0800

    update

--- ResponsiveApp/express.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-
-app.use(express.static(__dirname)); // Current directory is root
-
-app.listen(7000);
-console.log('Listening on port 7000');
