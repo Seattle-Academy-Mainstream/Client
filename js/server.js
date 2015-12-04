3ea45c3e6fdce063b45602efa11f8389b867dafd
Isaac Benson Zinda <isaaczinda@seattleacademy.org>
2015-12-03 17:57:09 -0800

    make paste work properly

--- js/server.js
+++ js/server.js
@@ -72,6 +72,18 @@ function UpdateLoginLogout()
 //once the document is ready
 $(document).ready(function()
 {
+	// intercept paste events to make sure that only plain text is pasted 
+	document.querySelector("div[contenteditable]").addEventListener("paste", function(e) {
+	    // cancel paste
+	    e.preventDefault();
+
+	    // get text representation of clipboard
+	    var text = e.clipboardData.getData("text/plain");
+
+	    // insert text manually
+	    document.execCommand("insertHTML", false, text);
+	});
+
 	//hide some things on the page load
 	$(".logged-in").hide(0);
 	$(".logged-out").hide(0);
@@ -297,4 +309,6 @@ function FullRender()
 
 	//add the <hr/> after every div
 	$(".post").after("<hr/>");
+
+
 }
